5a7b8db604ade9886be7a2ccb9fcbd340b3e7488
code.arpit <code.arpitjain@gmail.com>
2021-05-02 17:33:23 +0530

    udp in python

--- esp8266_udp/esp8266_udp.py
+++ esp8266_udp/esp8266_udp.py
@@ -1,20 +1,23 @@
 import socket
 
-
-ip = '' 
-port = 
+ip = '127.0.0.1' 
+port = 4220
 message = b' hi from python'
     
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-for i in range (1, 10):
-    sock.sendto(message, (ip ,port))
-    i += 1
+# for i in range (1, 10):
+#     sock.sendto(message, (ip ,port))
+#     i += 1
 
+# print(f'UDP target ip: {ip}')
+# print(f'UDP target port: {port}')
+# print (f'Message send: {message}')
 
+sock.bind((ip, port))
 
-print(f'UDP target ip: {ip}')
-print(f'UDP target port: {port}')
-print (f'Message send: {message}')
+while(True):
+    data, addr = sock.recvfrom(256)
+    print(f'received message: {data}')
 
