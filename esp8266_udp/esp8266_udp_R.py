44748bf3ad508d08ce1207902570c75abcc4eff3
code.arpit <code.arpitjain@gmail.com>
2021-05-02 18:18:06 +0530

    Send and receive in python

--- esp8266_udp/esp8266_udp_R.py
+++ esp8266_udp/esp8266_udp_R.py
@@ -1,23 +1,14 @@
 import socket
 
 ip = '127.0.0.1' 
-port = 4220
+port = 4293
 message = b' hi from python'
     
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# for i in range (1, 10):
-#     sock.sendto(message, (ip ,port))
-#     i += 1
-
-# print(f'UDP target ip: {ip}')
-# print(f'UDP target port: {port}')
-# print (f'Message send: {message}')
 
 sock.bind((ip, port))
 
 while(True):
     data, addr = sock.recvfrom(256)
     print(f'received message: {data}')
-
